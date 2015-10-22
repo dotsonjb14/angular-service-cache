dcb00f45234b18386927f6bec63e31647cb72830
Joseph Dotson <jdotson@ibs.com>
2015-10-22 19:12:48 -0400

    initial commit

---
+++ serviceCache.js
@@ -0,0 +1,29 @@
+(function () {
+    angular.module('service-cache')
+        .factory('serviceCache', function ($cacheFactory, $q) {
+            var cache = $cacheFactory("service-cache");
+            return {
+                run: _run,
+                remove: _remove
+            };
+
+            function _run(func, key) {
+                var val = cache.get(key);
+
+                if (typeof val !== 'undefined') {
+                    return $q.when(val);
+                }
+                else {
+                    return $q.when(func())
+                        .then(function (data) {
+                            cache.put(key, data);
+                            return data;
+                        });
+                }
+            }
+
+            function _remove(key) {
+                cache.remove(key);
+            }
+        });
+})();
