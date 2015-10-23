11f164d59c17b2fafdb8fc96c31e2057dfb08702
Joseph Dotson <jdotson@ibs.com>
2015-10-23 11:08:44 -0400

    used my own cache factory due to issues with

--- serviceCache.js
+++ serviceCache.js
@@ -1,12 +1,58 @@
+/**
+ * This module is used for cacheing services and the like.
+ *
+ * The reason I am not using $cacheFactory is because you cannot get a list of keys from there.
+ */
 (function () {
     angular.module('service-cache', [])
+        .factory('mCache', mCache)
         .factory('serviceCache', serviceCache);
 
-    serviceCache.$inject = ["$cacheFactory", "$q"];
-    function serviceCache ($cacheFactory, $q) {
-        var cache = $cacheFactory("service-cache");
+    function mCache() {
+        return function() {
+            var _cache = {};
+
+            return {
+                get: _get,
+                put: _put,
+                remove: _remove,
+                removeAll: _removeAll
+            };
+
+            function _get(key) {
+                return _cache[key];
+            }
+
+            function _put(key, val, ttl) {
+                _cache[key] = val;
+                if(typeof  ttl === 'undefined') {
+                    ttl = 0;
+                }
+                if(ttl > 0) {
+                    setTimeout(function () {
+                        _remove(key)
+                    }, ttl)
+                }
+            }
+
+            function _remove(key) {
+                delete _cache[key];
+            }
+
+            function _removeAll() {
+                _cache = {}; // reset it
+            }
+        }
+    }
+
+    serviceCache.$inject = ["mCache", "$q"];
+    function serviceCache (mCache, $q) {
+        var cache = mCache();
+
         return {
             run: _run,
+
+            // these are proxies for the cache backend
             put: _put,
             get: _get,
             remove: _remove
@@ -33,15 +79,7 @@
         }
 
         function _put(key, val, ttl) {
-            cache.put(key, val);
-            if(typeof  ttl === 'undefined') {
-                ttl = 0;
-            }
-            if(ttl > 0) {
-                setTimeout(function () {
-                    _remove(key)
-                }, ttl)
-            }
+            cache.put(key, val, ttl);
         }
 
         function _remove(key) {
