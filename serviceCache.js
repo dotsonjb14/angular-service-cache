de5dd5d65381169fef0461a4834626f9e24ae955
Joseph Dotson <jdotson@ibs.com>
2015-10-23 07:23:40 -0400

    moved ttl to _put, and exposed _put and _get methods

--- serviceCache.js
+++ serviceCache.js
@@ -7,14 +7,13 @@
         var cache = $cacheFactory("service-cache");
         return {
             run: _run,
+            put: _put,
+            get: _get,
             remove: _remove
         };
 
         function _run(func, key, ttl) {
-            var val = cache.get(key);
-            if(typeof  ttl === 'undefined') {
-                ttl = 0;
-            }
+            var val = _get(key);
 
             if (typeof val !== 'undefined') {
                 return $q.when(val);
@@ -22,19 +21,27 @@
             else {
                 return $q.when(func())
                     .then(function (data) {
-                        _put(key, data);
-                        if(ttl > 0) {
-                            setTimeout(function () {
-                                _remove(key)
-                            }, ttl)
-                        }
+                        _put(key, data, ttl);
+
                         return data;
                     });
             }
         }
 
-        function _put(key, val) {
+        function _get(key) {
+            return cache.get(key)
+        }
+
+        function _put(key, val, ttl) {
             cache.put(key, val);
+            if(typeof  ttl === 'undefined') {
+                ttl = 0;
+            }
+            if(ttl > 0) {
+                setTimeout(function () {
+                    _remove(key)
+                }, ttl)
+            }
         }
 
         function _remove(key) {
