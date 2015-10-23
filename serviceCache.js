30ee5821d5fce68d8c7dd6ce43ea8fc1913095de
Joseph Dotson <jdotson@ibs.com>
2015-10-22 20:36:45 -0400

    added proper injection

--- serviceCache.js
+++ serviceCache.js
@@ -1,37 +1,40 @@
 (function () {
     angular.module('service-cache', [])
-        .factory('serviceCache', function ($cacheFactory, $q) {
-            var cache = $cacheFactory("service-cache");
-            return {
-                run: _run,
-                remove: _remove
-            };
+        .factory('serviceCache', serviceCache());
 
-            function _run(func, key, ttl) {
-                var val = cache.get(key);
-                if(typeof  ttl === 'undefined') {
-                    ttl = 0;
-                }
+    serviceCache.$inject = ["$cacheFactory", "$q"]
+    function serviceCache ($cacheFactory, $q) {
+        var cache = $cacheFactory("service-cache");
+        return {
+            run: _run,
+            remove: _remove
+        };
 
-                if (typeof val !== 'undefined') {
-                    return $q.when(val);
-                }
-                else {
-                    return $q.when(func())
-                        .then(function (data) {
-                            cache.put(key, data);
-                            if(ttl > 0) {
-                                setTimeout(function () {
-                                    cache.remove(key)
-                                }, ttl)
-                            }
-                            return data;
-                        });
-                }
+        function _run(func, key, ttl) {
+            var val = cache.get(key);
+            if(typeof  ttl === 'undefined') {
+                ttl = 0;
             }
 
-            function _remove(key) {
-                cache.remove(key);
+            if (typeof val !== 'undefined') {
+                return $q.when(val);
             }
-        });
+            else {
+                return $q.when(func())
+                    .then(function (data) {
+                        cache.put(key, data);
+                        if(ttl > 0) {
+                            setTimeout(function () {
+                                cache.remove(key)
+                            }, ttl)
+                        }
+                        return data;
+                    });
+            }
+        }
+
+        function _remove(key) {
+            cache.remove(key);
+        }
+    }
 })();
